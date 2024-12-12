a25cf1b22cd26d83efc40477428d3c451710ebf3
Ashutosh <ashutoshmohanty13703@gmail.com>
2024-12-12 13:34:38 +0530

    added signup popup

--- src/components/AstrologerCardContainer.jsx
+++ src/components/AstrologerCardContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AstrologerCard from './AstrologerCrad'
 import data from './data'
+import UserSignup from './UserSignup'
 
 const AstrologerCardContainer = ({ sliceCount = 30 }) => {
 
@@ -13,6 +14,7 @@ const AstrologerCardContainer = ({ sliceCount = 30 }) => {
 
   return (
     <div>
+     <UserSignup/>
       <div className="px-[10px] lg:px-[65px] py-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
         {demo}
       </div>
